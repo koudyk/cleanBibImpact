d150be27ce6ff066254f5cbe22ff4e57b8d50f60
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-06-17 23:05:20 -0400

    visualize the percentages of citations in each category

--- src/visualization/visualize_manual_data.py
+++ src/visualization/visualize_manual_data.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -69,3 +70,74 @@ print(f'{n_preprints} preprints, {n_articles} journal articles')
 n_by_bassett = df['bassett_author'].sum()
 n_not_by_bassett = len(df) - n_by_bassett
 print(f'{n_by_bassett} including Dani Bassett as an author, {n_not_by_bassett} not')
+
+# # get the percentages from the diversity statement texts
+
+# possible labels depending on how many percentages are given
+possible_labels = {
+    4: ['mm', 'mf', 'fm', 'ff'],
+    5: ['mm', 'mf', 'fm', 'ff', 'unknown'],
+    6: ['mm', 'mf', 'fm', 'ff', 'nonbinary','unknown'],
+    8: ['mm', 'mf', 'fm', 'ff', 
+        'rel_mm', 'rel_mf', 'rel_fm', 'rel_ff'],
+    9: ['mm', 'mf', 'fm', 'ff', 
+        'rel_mm', 'rel_mf', 'rel_fm', 'rel_ff'],
+    10: ['mm', 'mf', 'fm', 'ff', 'unknown',
+        'rel_mm', 'rel_mf', 'rel_fm', 'rel_ff', 'relunknown'],
+    11: ['mm', 'mf', 'fm', 'ff', 'nonbinary', 'unknown',
+        'rel_mm', 'rel_mf', 'rel_fm', 'rel_ff', 'relunknown'],
+}
+
+# +
+import pprint
+import re
+
+pattern = '\−*\s*\d*.\d*\s*\%'
+for n, text in enumerate(df['ds_text']):    
+    strs = re.findall(pattern, text)
+    strs = [s.replace("−", "-") for s in strs]
+    strs = [re.sub('[A-Za-z,]', '', s) for s in strs]
+    
+    ints = [float(s[:-1]) for s in strs]
+    
+    if len(ints) > 0:
+        labels = possible_labels[len(ints)]    
+        d = dict(zip(labels, ints))
+    else: 
+        d = {}
+    df.loc[n, ('percentages')] = [d]
+            
+
+# +
+lists_percents = {'mm': [],
+                 'mf': [],
+                 'fm': [],
+                 'ff': [],
+}
+
+for d in df['percentages']:
+    if len(d) > 0:
+        for key in lists_percents.keys():
+            lists_percents[key].append(d[key])
+
+# +
+dataset = lists_percents.values()
+
+
+
+# +
+# %matplotlib inline
+import matplotlib
+matplotlib.rc('font', **{'size': 14})
+
+fig, axes = plt.subplots(figsize=(5,5))
+dataset = list(lists_percents.values())
+axes.violinplot(dataset=dataset)
+axes.set_ylabel('% of citations in paper')
+axes.set_xticks(range(len(lists_percents) + 1))
+ticks = ['', 'male\nmale', 'male\nfemale', 'female\nmale', 'female\nfemale']
+axes.set_xticklabels(ticks)
+plt.title('Citations in papers that\n' +\
+          'cite the paper on citation bias')
+plt.tight_layout()
+plt.savefig('../../reports/figures/violinplot_percentages.png')
